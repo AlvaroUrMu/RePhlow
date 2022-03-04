19b2b742fe10b61bcddbca3d57412c5dbc1ac681
AlvaroUrMu <alvarourmu@gmail.com>
2022-03-04 13:01:44 +0100

    Version1.2

--- app/src/main/java/org/ivz/ad/aurbano/rephlow/view/MainActivity.java
+++ app/src/main/java/org/ivz/ad/aurbano/rephlow/view/MainActivity.java
@@ -1,6 +1,8 @@
 package org.ivz.ad.aurbano.rephlow.view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.Toolbar;
 import androidx.cardview.widget.CardView;
 import androidx.lifecycle.LiveData;
@@ -12,6 +14,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.transition.AutoTransition;
 import android.transition.TransitionManager;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
@@ -76,5 +80,29 @@ public class MainActivity extends AppCompatActivity {
     private void openAddActivity(){
         Intent intent = new Intent(this, AddFloraActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int mode = AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES?AppCompatDelegate.MODE_NIGHT_NO:AppCompatDelegate.MODE_NIGHT_YES;
+
+        switch(item.getItemId()){
+            case  R.id.app_bar_switch:
+                if(mode == AppCompatDelegate.MODE_NIGHT_NO){
+                    AppCompatDelegate.setDefaultNightMode(mode);
+                } else if(mode == AppCompatDelegate.MODE_NIGHT_YES){
+                    AppCompatDelegate.setDefaultNightMode(mode);
+                }
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
